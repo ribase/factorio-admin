6b82368d09ebcf8e438dc655c2886ac6b79ac7f6
sebastian <baschdi@wondrous.ch>
2017-02-17 12:21:49 +0100

    fixed updater

--- factorio/firstinstall.sh
+++ factorio/firstinstall.sh
@@ -6,9 +6,9 @@ cd update-folder/ && tar -xf $1
 
 cd ../
 
-cp -rf ./update-folder/factorio/* ../factorio
+cp -rf update-folder/factorio/* ../factorio
 
-rm -rf ./update-folder/*
+rm -rf update-folder/*
 
 return true
 
